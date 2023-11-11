309e9217a97c42f39b2bd3a3dfa74e96e568dc7c
Spleenagon <andrewsp@iinet.net.au>
2023-11-11 15:07:01 +0800

    Automated Update

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '212434379342791515791581089342858464091'                       // used to check whether the localStorage data is stale
+var gzip_hash = '251730204396515821887831398208191458186'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
